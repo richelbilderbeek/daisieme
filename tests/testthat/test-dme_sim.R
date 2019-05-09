5b94b696b826a1c3fc1dd5032afeca7e913a9374
richelbilderbeek <richel@richelbilderbeek.nl>
2019-05-09 14:15:11 +0200

    Run DAISIE

--- tests/testthat/test-dme_sim.R
+++ tests/testthat/test-dme_sim.R
@@ -21,27 +21,25 @@ test_that("use", {
   daisie_output_ideal_filename <- tempfile()
   daisie_output_reality_filename <- tempfile()
 
-  expect_silent(
-    dme_sim(
-      ana_psr = ana_psr,
-      clado_is_psr = clado_is_psr,
-      clado_main_psr = clado_main_psr,
-      ext_is_psr = ext_is_psr,
-      ext_main_psr = ext_main_psr,
-      mig_to_is_psr = mig_to_is_psr,
-      carry_cap_is = carry_cap_is,
-      carry_cap_main = carry_cap_main,
-      rng_seed = rng_seed,
-      init_n_main_clades = init_n_main_clades,
-      init_n_main_sps = init_n_main_sps,
-      crown_age = crown_age,
-      dme_params_filename = dme_params_filename,
-      daisie_sim_results_filename = daisie_sim_results_filename,
-      daisie_input_ideal_filename = daisie_input_ideal_filename,
-      daisie_input_reality_filename = daisie_input_reality_filename,
-      daisie_output_ideal_filename = daisie_output_ideal_filename,
-      daisie_output_reality_filename = daisie_output_reality_filename
-    )
+  dme_sim(
+    ana_psr = ana_psr,
+    clado_is_psr = clado_is_psr,
+    clado_main_psr = clado_main_psr,
+    ext_is_psr = ext_is_psr,
+    ext_main_psr = ext_main_psr,
+    mig_to_is_psr = mig_to_is_psr,
+    carry_cap_is = carry_cap_is,
+    carry_cap_main = carry_cap_main,
+    rng_seed = rng_seed,
+    init_n_main_clades = init_n_main_clades,
+    init_n_main_sps = init_n_main_sps,
+    crown_age = crown_age,
+    dme_params_filename = dme_params_filename,
+    daisie_sim_results_filename = daisie_sim_results_filename,
+    daisie_input_ideal_filename = daisie_input_ideal_filename,
+    daisie_input_reality_filename = daisie_input_reality_filename,
+    daisie_output_ideal_filename = daisie_output_ideal_filename,
+    daisie_output_reality_filename = daisie_output_reality_filename
   )
   expect_true(file.exists(dme_params_filename))
   expect_true(file.exists(daisie_sim_results_filename))
