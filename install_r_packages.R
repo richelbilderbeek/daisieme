3a9e5e6c6a838e3b0a425365ce5a3a90ce7250d7
richelbilderbeek <richel@richelbilderbeek.nl>
2019-05-09 09:42:54 +0200

    Install other R packages

--- install_r_packages.R
+++ install_r_packages.R
@@ -4,10 +4,14 @@ my_repo <- "http://cran.us.r-project.org"
 install.packages("devtools", repos = my_repo)
 
 # v1.2, does not support Endemic&NonEndemic yet
-install.packages("DAISIE", repos = my_repo)
+#install.packages("DAISIE", repos = my_repo)
 # v1.3, does support Endemic&NonEndemic, but is closed
 # devtools::install_github("richelbilderbeek/DAISIE_1_3", subdir = "DAISIE")
+devtools::install_github("rsetienne/DAISIE")
 
 devtools::install_github("richelbilderbeek/nLTT")
-devtools::install_github("rsetienne/PBD", subdir = "PBD")
+
+devtools::install_github("richelbilderbeek/DDD")
+#devtools::install_github("richelbilderbeek/PBD", subdir = "PBD")
+#devtools::install_github("rsetienne/PBD", subdir = "PBD")
 warnings()
