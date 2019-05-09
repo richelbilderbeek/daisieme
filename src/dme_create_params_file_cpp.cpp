6ae023f02236c4e7e8d25355b0091d1e6815aff5
richelbilderbeek <richel@richelbilderbeek.nl>
2019-05-09 11:00:20 +0200

    First test

--- src/dme_create_params_file_cpp.cpp
+++ src/dme_create_params_file_cpp.cpp
@@ -5,5 +5,5 @@
 void dme_create_params_file_cpp(const std::string& filename)
 {
   std::ofstream f(filename.c_str());
- f << "STUB";
+  f << "STUB";
 }
--- tests/testthat/test-dme_create_params_file.R
+++ tests/testthat/test-dme_create_params_file.R
@@ -1,5 +1,8 @@
 context("test-dme_create_params_file")
 
 test_that("use", {
-  expect_silent(dme_create_params_file(tempfile()))
+
+  filename <- tempfile()
+  expect_silent(dme_create_params_file(filename = filename))
+  expect_true(file.exists(filename))
 })
