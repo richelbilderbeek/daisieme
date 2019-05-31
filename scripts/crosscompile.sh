d2c16706f223e28119e827a87e2ca5d45a89b2fa
richelbilderbeek <richel@richelbilderbeek.nl>
2019-05-31 09:43:49 +0200

    First start of crosscompile script [skip ci]

---
+++ scripts/crosscompile.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# Crosscompiles daisieme C++GUI to the Windows operating system
+#
+# Usage:
+#
+#   ./crosscompile.sh
+#
+DESKTOP_PRO=elly_gui.pro
+
+cd ../cpp
+
+if [ ! -e $DESKTOP_PRO ]
+then
+  echo "ERROR: Cannot find "$DESKTOP_PRO
+  exit
+fi
+i686-w64-mingw32.static-qmake-qt5 $DESKTOP_PRO
+make
+cd ..
+
+DESKTOP_EXE=./../cpp/release/elly_gui.exe
+
+if [ ! -e $DESKTOP_EXE ]
+then
+  echo "ERROR: Cannot find "$DESKTOP_EXE
+else
+  echo "OK: Can find "$DESKTOP_EXE
+fi
+
+
+mv $DESKTOP_EXE daisieme.exe
